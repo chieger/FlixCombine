376fdbae7b60200bd48dd53ff3b739225dee8903
Charlie Hieger <charlie@velosmobile.com>
2020-04-15 17:53:32 -0700

    Add ImageLoader and AsyncImage files

---
+++ FlixCombine/New Group/ImageLoader.swift
@@ -0,0 +1,9 @@
+//
+//  ImageLoader.swift
+//  FlixCombine
+//
+//  Created by Charles Hieger on 4/15/20.
+//  Copyright © 2020 Charles Hieger. All rights reserved.
+//
+
+import Foundation
---
+++ FlixCombine/View/AsyncImage.swift
@@ -0,0 +1,21 @@
+//
+//  AsyncImage.swift
+//  FlixCombine
+//
+//  Created by Charles Hieger on 4/15/20.
+//  Copyright © 2020 Charles Hieger. All rights reserved.
+//
+
+import SwiftUI
+
+struct AsyncImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AsyncImage_Previews: PreviewProvider {
+    static var previews: some View {
+        AsyncImage()
+    }
+}
